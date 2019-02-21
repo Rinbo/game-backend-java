b8e5dd1d331776f9fbd3ad3ca9ea17aa3dc13cec
Rinbo <robin.b@outlook.com>
2019-02-21 14:10:08 +0100

    First commit

---
+++ src/main/java/nu/borjessons/web/game_backend/App.java
@@ -0,0 +1,13 @@
+package nu.borjessons.web.game_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
