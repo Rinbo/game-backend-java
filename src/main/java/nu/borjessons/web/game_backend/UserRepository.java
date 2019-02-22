78b1cabbd3084825e969338e2d4999f1e737eea1
Rinbo <robin.b@outlook.com>
2019-02-22 09:20:09 +0100

    Add application.properties for connection to database

---
+++ src/main/java/nu/borjessons/web/game_backend/UserRepository.java
@@ -0,0 +1,11 @@
+package nu.borjessons.web.game_backend;
+
+import org.springframework.data.repository.CrudRepository;
+import nu.borjessons.web.game_backend.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
